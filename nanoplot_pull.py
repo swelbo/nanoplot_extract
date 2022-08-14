988cf2670df497770a93eac5418dc29829c18aba
Thomas Sewell <tsewell000@gmaail.com>
2022-08-14 11:21:08 +0100

    modifications to script

--- nanoplot_pull.py
+++ nanoplot_pull.py
@@ -2,19 +2,19 @@
 # (which can then be concat to a master .csv file
 
 # Import pandas
-#import pandas as pd
-# read txt file
-#df = pd.read_fwf('NanoStats.txt')
-# write file to csv
-#df.to_csv('log.csv')
-
-#with open('test.txt') as file:
-	#first_line = file.readlines()[2:4]
-#print(first_line)
-
 from itertools import islice
-with open('NanoStats.txt') as file:
-    for read_length in islice(file, 1, 2):
-        print(read_length[31:39])
 
-print(read_length[31:39])
+# Read length
+with open('NanoStats.txt') as file:
+    for read_length in islice(file,1,2):
+        print(float(read_length[31:39].replace(",","")))
+
+with open('NanoStats.txt') as file:
+    for y in islice(file,2,3):
+        print(y[35:39])
+
+x = read_length[31:39] + " " + y[35:39]
+x = print(x)
+#with open('NanoStats.txt') as file:
+    #for y in islice(file, 3, 4):
+        #print(y[35:39])
