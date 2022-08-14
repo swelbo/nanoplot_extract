71efabe4df1ab9810b13d7a4bba168c5e72a0dd1
Thomas Sewell <tsewell000@gmaail.com>
2022-08-14 11:41:25 +0100

    continued updates

--- nanoplot_pull.py
+++ nanoplot_pull.py
@@ -1,20 +1,31 @@
 # Program to pull data from Nanoplot run and populate csv file
 # (which can then be concat to a master .csv file
 
-# Import pandas
+# Import itertools
 from itertools import islice
 
-# Read length
+# Mean read length
 with open('NanoStats.txt') as file:
-    for read_length in islice(file,1,2):
-        print(float(read_length[31:39].replace(",","")))
+    for mean_length in islice(file,1,2):
+        print(float(mean_length[31:39].replace(",","")))
 
+# Mean read quality
 with open('NanoStats.txt') as file:
-    for y in islice(file,2,3):
-        print(y[35:39])
+    for mean_quality in islice(file,2,3):
+        print(float(mean_quality[35:39]))
 
-x = read_length[31:39] + " " + y[35:39]
+# Median read length
+with open('NanoStats.txt') as file:
+    for med_length in islice(file,3,4):
+        print(float(med_length[31:39].replace(",","")))
+
+# Median read quality
+with open('NanoStats.txt') as file:
+    for med_quality in islice(file,4,5):
+        print(med_quality[35:39])
+
+
+x = mean_length[31:39] + " " + mean_quality[35:39] 
+
+
 x = print(x)
-#with open('NanoStats.txt') as file:
-    #for y in islice(file, 3, 4):
-        #print(y[35:39])
