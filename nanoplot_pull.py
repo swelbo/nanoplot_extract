11a201d91432a373b7ed32badf09c428d15e5b7e
Thomas Sewell <tsewell000@gmaail.com>
2022-08-15 11:23:40 +0100

    update to include all data

--- nanoplot_pull.py
+++ nanoplot_pull.py
@@ -7,25 +7,42 @@ from itertools import islice
 # Mean read length
 with open('NanoStats.txt') as file:
     for mean_length in islice(file,1,2):
-        print(float(mean_length[31:39].replace(",","")))
+        a = (float(mean_length[25:39].replace(",","")))
 
 # Mean read quality
 with open('NanoStats.txt') as file:
     for mean_quality in islice(file,2,3):
-        print(float(mean_quality[35:39]))
+        b = (float(mean_quality[34:39]))
 
 # Median read length
 with open('NanoStats.txt') as file:
     for med_length in islice(file,3,4):
-        print(float(med_length[31:39].replace(",","")))
+        c = (float(med_length[30:39].replace(",","")))
 
 # Median read quality
 with open('NanoStats.txt') as file:
     for med_quality in islice(file,4,5):
-        print(med_quality[35:39])
+        d = (float(med_quality[30:39].replace(",","")))
 
+# Number of reads:
+with open('NanoStats.txt') as file:
+    for no_reads in islice(file,5,6):
+        e = (float(no_reads[29:39].replace(",","")))
 
-x = mean_length[31:39] + " " + mean_quality[35:39] 
+# Read length N50:
+with open('NanoStats.txt') as file:
+    for STDEV in islice(file,6,7):
+        f = (float(STDEV[29:39].replace(",","")))
 
+# Number of reads:
+with open('NanoStats.txt') as file:
+    for no_reads in islice(file,7,8):
+        g = (float(no_reads[29:39].replace(",","")))
 
-x = print(x)
+# Number of bases
+with open('NanoStats.txt') as file:
+    for total_bases in islice(file,8,9):
+         h = (float(total_bases[25:39].replace(",","")))
+
+#x = mean_length[31:39] + " " + mean_quality[35:39] 
+print(type(a))#,b,c,d,e,f,g,h)
